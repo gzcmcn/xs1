0dfbd0527a4121aaf019c7db0c09f96e946aaf84
gzcmcn <guzhucanmeng@163.com>
2025-03-17 14:26:58 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,12 +3,12 @@ const CHAPTERS = [
     { file: 'chapter1.md', title: '第一章：双星·降世' },
     { file: 'chapter2.md', title: '第二章：星力初显' },
     { file: 'chapter3.md', title: '第三章：江天航' }
-    { file: 'chapter4.md', title: '第三章：江天航' }
-{ file: 'chapter5.md', title: '第三章：江天航' }
-{ file: 'chapter6.md', title: '第三章：江天航' }
-{ file: 'chapter7.md', title: '第三章：江天航' }
-{ file: 'chapter8.md', title: '第三章：江天航' }
-{ file: 'chapter9.md', title: '第三章：江天航' }
-{ file: 'chapter10.md', title: '第三章：江天航' }
-{ file: 'chapter11.md', title: '第三章：江天航' }
+    { file: 'chapter4.md', title: '第四章：鹊的一天' }
+{ file: 'chapter5.md', title: '第五章：复仇' }
+{ file: 'chapter6.md', title: '第六章：科技星石' }
+{ file: 'chapter7.md', title: '第七章：双星·结' }
+{ file: 'chapter8.md', title: '第八章：双星·聚' }
+{ file: 'chapter9.md', title: '第九章：双星·动' }
+{ file: 'chapter10.md', title: '第十章：' }
+{ file: 'chapter11.md', title: '第十一章：' }
 ];
