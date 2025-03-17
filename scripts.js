280a2d7cfaaafdf3e4701a3263a30d692ad71eab
gzcmcn <guzhucanmeng@163.com>
2025-03-17 14:22:48 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 // 章节列表
 const CHAPTERS = [
-    { file: 'chapter1.md', title: '第一章：初遇' },
-    { file: 'chapter2.md', title: '第二章：冒险开始' },
-    { file: 'chapter3.md', title: '第三章：神秘之地' }
+    { file: 'chapter1.md', title: '第一章：双星·降世' },
+    { file: 'chapter2.md', title: '第二章：星力初显' },
+    { file: 'chapter3.md', title: '第三章：江天航' }
+    { file: 'chapter4.md', title: '第三章：江天航' }
+{ file: 'chapter5.md', title: '第三章：江天航' }
+{ file: 'chapter6.md', title: '第三章：江天航' }
+{ file: 'chapter7.md', title: '第三章：江天航' }
+{ file: 'chapter8.md', title: '第三章：江天航' }
+{ file: 'chapter9.md', title: '第三章：江天航' }
+{ file: 'chapter10.md', title: '第三章：江天航' }
+{ file: 'chapter11.md', title: '第三章：江天航' }
 ];
